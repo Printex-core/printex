bb6c472ea0086292b1448c6883c74320007d2ea1
Printex-core <39118235+Printex-core@users.noreply.github.com>
2018-05-12 17:10:16 +0200

    Add files via upload

---
+++ share/qt/make_windows_icon.sh
@@ -0,0 +1,5 @@
+#!/bin/ferrum
+# create multiresolution windows icon
+ICON_DST=../../src/qt/res/icons/novacoin.ico
+
+convert ../../src/qt/res/icons/novacoin-16.png ../../src/qt/res/icons/novacoin-32.png ../../src/qt/res/icons/novacoin-48.png ${ICON_DST}
